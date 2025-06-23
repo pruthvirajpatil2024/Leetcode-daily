980c0022c4e25217679195e942a00950bfb24fae
Pruthviraj Patil <137247110+pruthvirajpatil2024@users.noreply.github.com>
2025-06-23 21:25:27 +0530

    Approach Modified

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        for (int i = 0; i < n - 1; ++i) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i] <<= 1;
+        vector<int> temp;
+
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && nums[i] == nums[i + 1]) {
+                temp.push_back(nums[i] * 2);
                 nums[i + 1] = 0;
+                ++i;
+            } else if (nums[i] != 0) {
+                temp.push_back(nums[i]);
             }
         }
-        vector<int> ans(n);
-        int i = 0;
-        for (int& x : nums) {
-            if (x) {
-                ans[i++] = x;
-            }
+
+        while (temp.size() < n) {
+            temp.push_back(0);
         }
-        return ans;
+
+        return temp;
     }
 };
